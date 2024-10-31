c6317ddaabbee865f0b4f7f67a57b003a468e879
sdybybhon <artem563732@mail.ru>
2024-10-31 19:01:11 +0500

    Уфимцев Артём. Задание 06

--- task06/src/com/example/task06/Task06Main.java
+++ task06/src/com/example/task06/Task06Main.java
@@ -3,14 +3,16 @@ package com.example.task06;
 public class Task06Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        new Task06Main().printMethodName();
-         */
-    }
 
+        new Task06Main().printMethodName();
+
+    }
+    //тесты не проходит
     void printMethodName() {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        Exception ex = new Exception("Message");
+        StackTraceElement[] elements = ex.getStackTrace();
+
+        System.out.println(elements[1].getMethodName());
     }
 
 }
